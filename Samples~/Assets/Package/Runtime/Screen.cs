af6f95fde0eeac19e5d087c8e06c15db2578cd29
dongl1n <dongl1n777@qq.com>
2023-03-19 11:57:11 +0800

    [add]add panel for ui

--- Samples~/Assets/Package/Runtime/Screen.cs
+++ Samples~/Assets/Package/Runtime/Screen.cs
@@ -9,26 +9,11 @@ namespace UnityTerminal
         public RenderTerminal terminal = null;
 
         /// Whether this screen is bound to a [RenderTerminal].
-        ///
-        /// If this is `false`, then [terminal] cannot be accessed.
         bool isBind => terminal != null;
 
         /// Whether this screen allows any screens under it to be visible.
-        ///
-        /// Subclasses can override this. Defaults to `false`.
         public bool isTransparent = false;
 
-        // public bool isTop
-        // {
-        //     get 
-        //     {
-        //         if (terminal == null)
-        //             return false;
-        //         return terminal.IsTopScreen(this);
-        //     }
-        // }
-
-        /// Binds this screen to [terminal].
         public void Bind(RenderTerminal tel)
         {
             Debug.Assert(terminal == null);
@@ -37,17 +22,12 @@ namespace UnityTerminal
             Resize(terminal.width, terminal.height);
         }
 
-        /// Unbinds this screen from the [terminal] that owns it.
         public void Unbind()
         {
             Debug.Assert(terminal != null);
             terminal = null;
         }
 
-        /// Marks the user interface as needing to be rendered.
-        ///
-        /// Call this during [update] to indicate that a subsequent call to [render]
-        /// is needed.
         public void Dirty()
         {
             // If we aren't bound (yet), just do nothing. The screen will be dirtied
@@ -61,11 +41,14 @@ namespace UnityTerminal
         /// screen is now the top-most screen. If a value was passed to [pop()], it
         /// will be passed to this as [result].
         public virtual void Activate(Screen popped, object result) { }
+        public virtual void UnActive(Screen last) { }
         public virtual void Tick(float dt) { }
         public virtual void HandleInput() { }
         public virtual void Render(Terminal terminal) { }
         /// Called when the [UserInterface] has been bound to a new terminal with a
         /// different size while this [Screen] is present.
         public virtual void Resize(int width, int height) { }
+        public virtual void OnBind() { }
+        public virtual void OnUnBind() { }
     }
 }
--- Samples~/Assets/Test/Test.cs
+++ Samples~/Assets/Test/Test.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityTerminal;
+using Screen = UnityTerminal.Screen;
 
-class DialogScreen : UnityTerminal.Screen {
-  
+class PlayerInfoPanel : Panel {
+
 }
 
-class MainScreen : UnityTerminal.Screen {
+class HudPanel : Panel {
+    public override void OnBind()
+    {
+        base.OnBind();
+
+        
+    }
+}
+
+class MainScreen : Screen {
   // public List<Ball> balls = new List<Ball>();
 
   public MainScreen() {
@@ -83,66 +93,6 @@ class MainScreen : UnityTerminal.Screen {
     // terminal.writeAt(59, 0, "switch terminal [tab]", ColorX.darkGray);
     // terminal.writeAt(75, 0, "[tab]", ColorX.lightGray);
     return;
-
-    // terminal.clear();
-
-    // void colorBar(int y, string name, Color light, Color medium, Color dark) {
-    //   terminal.writeAt(2, y, name, Color.gray);
-    //   terminal.writeAt(10, y, "light", light);
-    //   terminal.writeAt(16, y, "medium", medium);
-    //   terminal.writeAt(23, y, "dark", dark);
-
-    //   terminal.writeAt(28, y, " light ", Color.black, light);
-    //   terminal.writeAt(35, y, " medium ", Color.black, medium);
-    //   terminal.writeAt(43, y, " dark ", Color.black, dark);
-    // }
-
-    // terminal.writeAt(0, 0, "Predefined colors:");
-    // terminal.writeAt(59, 0, "switch terminal [tab]", Color.darkGray);
-    // terminal.writeAt(75, 0, "[tab]", Color.lightGray);
-    // colorBar(1, "gray", Color.lightGray, Color.gray, Color.darkGray);
-    // colorBar(2, "red", Color.lightRed, Color.red, Color.darkRed);
-    // colorBar(3, "orange", Color.lightOrange, Color.orange, Color.darkOrange);
-    // colorBar(4, "gold", Color.lightGold, Color.gold, Color.darkGold);
-    // colorBar(5, "yellow", Color.lightYellow, Color.yellow, Color.darkYellow);
-    // colorBar(6, "green", Color.lightGreen, Color.green, Color.darkGreen);
-    // colorBar(7, "aqua", Color.lightAqua, Color.aqua, Color.darkAqua);
-    // colorBar(8, "blue", Color.lightBlue, Color.blue, Color.darkBlue);
-    // colorBar(9, "purple", Color.lightPurple, Color.purple, Color.darkPurple);
-    // colorBar(10, "brown", Color.lightBrown, Color.brown, Color.darkBrown);
-
-    // terminal.writeAt(0, 12, "Code page 437:");
-    // var lines = new string[]{
-    //   " ☺☻♥♦♣♠•◘○◙♂♀♪♫☼",
-    //   "►◄↕‼¶§▬↨↑↓→←∟↔▲▼",
-    //   " !\"#\\$%&'()*+,-./",
-    //   "0123456789:;<=>?",
-    //   "@ABCDEFGHIJKLMNO",
-    //   "PQRSTUVWXYZ[\\]^_",
-    //   "`abcdefghijklmno",
-    //   "pqrstuvwxyz{|}~⌂",
-    //   "ÇüéâäàåçêëèïîìÄÅ",
-    //   "ÉæÆôöòûùÿÖÜ¢£¥₧ƒ",
-    //   "áíóúñÑªº¿⌐¬½¼¡«»",
-    //   "░▒▓│┤╡╢╖╕╣║╗╝╜╛┐",
-    //   "└┴┬├─┼╞╟╚╔╩╦╠═╬╧",
-    //   "╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀",
-    //   "αßΓπΣσµτΦΘΩδ∞φε∩",
-    //   "≡±≥≤⌠⌡÷≈°∙·√ⁿ²■"
-    // };
-
-    // var y = 13;
-    // foreach (var line in lines) {
-    //   terminal.writeAt(3, y++, line, Color.lightGray);
-    // }
-
-    // terminal.writeAt(22, 12, "Simple game loop:");
-    // terminal.writeAt(66, 12, "toggle [space]", Color.darkGray);
-    // terminal.writeAt(73, 12, "[space]", Color.lightGray);
-
-    // foreach (var ball in balls) {
-    //   ball.render(terminal);
-    // }
   }
 }
 
